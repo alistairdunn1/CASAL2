399d96e70f2f76c1cea029a161458954ed3f8dd3
Alistair Dunn <alistairdunn1@gmail.com>
2023-09-28 11:12:01 +1300

    UPD: Update generate.starting.pars to work with MPD not config files; Minor r .rd file edits; Rebuild

--- R-libraries/casal2/R/extract.parameters.R
+++ R-libraries/casal2/R/extract.parameters.R
@@ -1,9 +1,9 @@
 #' Utility extract.parameters function
 #'
-#' This function reads in a parameter file that would be generated using the -o syntax.
+#' This function reads in a free parameter file that would be generated using the -o syntax.
 #'
 #' @author Craig Marsh
-#' @param file The name of the input file containing model output to extract
+#' @param file The name of the free parameter file containing model output to extract
 #' @param path The path to the file (optional)
 #' @param fileEncoding Switch to read in files that have been encoded in alternative UTF formats (optional); see the Casal2 User Manual for the error message that would indicate when to use this switch.
 #' @param quiet suppress print or cat statements to screen.
