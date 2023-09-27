14bf9327022b81d480d17a5cdf880161d5b4f83b
Alistair Dunn <alistairdunn1@gmail.com>
2023-09-28 10:48:31 +1300

    FIX: Bounds on constant selectivity parameters

--- CASAL2/source/Selectivities/Common/Constant.cpp
+++ CASAL2/source/Selectivities/Common/Constant.cpp
@@ -19,8 +19,8 @@ namespace selectivities {
  * Default constructor
  */
 Constant::Constant(shared_ptr<Model> model) : Selectivity(model) {
-  parameters_.Bind<Double>(PARAM_A, &a_, "The constant value (default a = 0) in ax^b + c", "", 0.0);
-  parameters_.Bind<Double>(PARAM_B, &b_, "The constant value (default b = 0) in ax^b + c", "", 0.0)->set_lower_bound(0.0, true);
+  parameters_.Bind<Double>(PARAM_A, &a_, "The constant value (default a = 0) in ax^b + c", "", 0.0)->set_lower_bound(0.0, true);
+  parameters_.Bind<Double>(PARAM_B, &b_, "The constant value (default b = 0) in ax^b + c", "", 0.0);
   parameters_.Bind<Double>(PARAM_C, &c_, "The constant value (default c = 1) in ax^b + c", "", 1.0)->set_lower_bound(0.0, true);
   parameters_.Bind<Double>(PARAM_BETA, &beta_, "The minimum age for which the selectivity applies", "", 0.0)->set_lower_bound(0.0, true);
 
