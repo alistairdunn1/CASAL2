38a7bbae501f9d1d6fa322c053c56a47326f102d
Scott Rasmussen (Zaita) <scott@zaita.com>
2025-03-26 21:03:14 +1300

    CHG: Code clean up on DQ.Biomass

--- CASAL2/source/DerivedQuantities/Age/Biomass.cpp
+++ CASAL2/source/DerivedQuantities/Age/Biomass.cpp
@@ -11,16 +11,14 @@
 // headers
 #include "../../DerivedQuantities/Age/Biomass.h"
 
+#include "../../AgeLengths/AgeLength.h"
 #include "../../AgeWeights/Manager.h"
 #include "../../InitialisationPhases/Manager.h"
 #include "../../TimeSteps/Manager.h"
-#include "../../AgeLengths/AgeLength.h"
 #include "../../Utilities/Math.h"
 
 // namespaces
-namespace niwa {
-namespace derivedquantities {
-namespace age {
+namespace niwa::derivedquantities::age {
 
 namespace math = utilities::math;
 /**
@@ -77,8 +75,8 @@ void Biomass::PreExecute() {
         unsigned age = (*iterator)->min_age_ + j;
         cache_value_ += (*iterator)->data_[j] * selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) * (*iterator)->age_length_->mean_weight(time_step_index, age);
         LOG_FINEST() << "Biomass (Pre-execute) for category = " << (*iterator)->name_ << " age = " << age
-                   << " mean weight = " << (*iterator)->age_length_->mean_weight(time_step_index, age)
-                   << " selectivity = " << selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) << " numbers = " << (*iterator)->data_[j];
+                     << " mean weight = " << (*iterator)->age_length_->mean_weight(time_step_index, age)
+                     << " selectivity = " << selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) << " numbers = " << (*iterator)->data_[j];
       }
     }
   } else {
@@ -87,8 +85,8 @@ void Biomass::PreExecute() {
         unsigned age = (*iterator)->min_age_ + j;
         cache_value_ += (*iterator)->data_[j] * selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) * age_weights_[i]->mean_weight_at_age_by_year(year, age);
         LOG_FINEST() << "Biomass (Pre-execute) for category = " << (*iterator)->name_ << " age = " << age
-                   << " mean weight = " << age_weights_[i]->mean_weight_at_age_by_year(year, age)
-                   << " selectivity = " << selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) << " numbers = " << (*iterator)->data_[j];
+                     << " mean weight = " << age_weights_[i]->mean_weight_at_age_by_year(year, age)
+                     << " selectivity = " << selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) << " numbers = " << (*iterator)->data_[j];
       }
     }
   }
@@ -120,7 +118,7 @@ void Biomass::Execute() {
         for (unsigned j = 0; j < (*iterator)->data_.size(); ++j) {
           unsigned age = (*iterator)->min_age_ + j;
           LOG_FINEST() << "Biomass for category = " << (*iterator)->name_ << " age = " << age << " mean weight = " << (*iterator)->age_length_->mean_weight(time_step_index, age)
-                     << " selectivity = " << selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) << " numbers = " << (*iterator)->data_[j];
+                       << " selectivity = " << selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) << " numbers = " << (*iterator)->data_[j];
           value += (*iterator)->data_[j] * selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) * (*iterator)->age_length_->mean_weight(time_step_index, age);
         }
       }
@@ -130,7 +128,7 @@ void Biomass::Execute() {
         for (unsigned j = 0; j < (*iterator)->data_.size(); ++j) {
           unsigned age = (*iterator)->min_age_ + j;
           LOG_FINEST() << "Biomass for category = " << (*iterator)->name_ << " age = " << age << " mean weight = " << age_weights_[i]->mean_weight_at_age_by_year(year, age)
-                     << " selectivity = " << selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) << " numbers = " << (*iterator)->data_[j];
+                       << " selectivity = " << selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) << " numbers = " << (*iterator)->data_[j];
           value += (*iterator)->data_[j] * selectivities_[i]->GetAgeResult(age, (*iterator)->age_length_) * age_weights_[i]->mean_weight_at_age_by_year(year, age);
         }
       }
@@ -185,9 +183,8 @@ void Biomass::Execute() {
     else
       values_[model_->current_year()] = pow(cache_value_, 1 - time_step_proportion_) * pow(value, time_step_proportion_);
   }
-  LOG_FINE() << " Pre Exploitation value " << cache_value_ << " time step proportion " << time_step_proportion_ << " Post exploitation " << value << " Final value " << values_[model_->current_year()];
+  LOG_FINE() << " Pre Exploitation value " << cache_value_ << " time step proportion " << time_step_proportion_ << " Post exploitation " << value << " Final value "
+             << values_[model_->current_year()];
 }
 
-} /* namespace age */
-} /* namespace derivedquantities */
-} /* namespace niwa */
+}  // namespace niwa::derivedquantities::age
